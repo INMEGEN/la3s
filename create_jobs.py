4af074e4940be99e9a6d681e23f211f6d181f782
absorto <rgarcia@inmegen.gob.mx>
2014-11-20 16:31:01 -0600

    write to file instead of printing

--- create_jobs.py
+++ create_jobs.py
@@ -10,17 +10,18 @@ with open("%s/segments.json" % config.data_dir,'r') as f:
 
 loci = sorted(segments.keys())
 
-for i in range(0,len(loci)):
+for i in range(0,len(loci)-1):
     (chrom, from_kb)    = loci[i].split(':')
     (next_chrom, to_kb) = loci[i+1].split(':')
     if chrom == next_chrom:
-        print segment_template.render( plink   = config.plink,
-                                       emmax   = config.emmax,
-                                       metal   = config.metal,
-                                       python  = config.python,
-                                       inputs     = config.inputs,
-                                       segments = "%s/segments.json" % config.data_dir,
-                                       chrom      = chrom,
-                                       from_kb    = from_kb,
-                                       to_kb      = to_kb, )
+        with open("%s_%s.job" % (chrom,from_kb), "w") as f:
+            f.write( segment_template.render( plink   = config.plink,
+                                              emmax   = config.emmax,
+                                              metal   = config.metal,
+                                              python  = config.python,
+                                              inputs     = config.inputs,
+                                              segments = "%s/segments.json" % config.data_dir,
+                                              chrom      = chrom,
+                                              from_kb    = from_kb,
+                                              to_kb      = to_kb, ) )
 
