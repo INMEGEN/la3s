c5448aa2532e0492873241943b3479a161db7e99
absorto <rgarcia@inmegen.gob.mx>
2014-11-20 15:44:25 -0600

    LSF job file creation script

---
+++ create_jobs.py
@@ -0,0 +1,28 @@
+from jinja2 import Template
+import json
+
+import config 
+
+segment_template = Template(open('per_segment.template','r').read())
+
+with open("%s/segments.json" % config.data_dir,'r') as f:
+    segments = json.load(f)
+
+loci = sorted(segments.keys())
+
+for i in range(0,len(loci)):
+    (chrom, from_kb)    = loci[i].split(':')
+    (next_chrom, to_kb) = loci[i+1].split(':')
+    if chrom == next_chrom:
+        for population in config.inputs:
+            print segment_template.render( plink   = config.plink,
+                                           emmax   = config.emmax,
+                                           metal   = config.metal,
+                                           python  = config.python,
+                                           population = population,
+                                           inputs     = config.inputs[population],
+                                           segments = "%s/segments.json" % config.data_dir,
+                                           chrom      = chrom,
+                                           from_kb    = from_kb,
+                                           to_kb      = to_kb, )
+
