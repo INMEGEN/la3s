03f43d85a868a7e88b5c8c96ab35ae53c4eb51c6
absorto <rgarcia@inmegen.gob.mx>
2014-11-24 18:25:46 -0600

    Not kb, bp. Use this dir for la3sdir.

--- create_jobs.py
+++ create_jobs.py
@@ -1,9 +1,12 @@
 from jinja2 import Template
 import json
+import os
 
 import config 
 
-segment_template = Template(open('per_segment.template','r').read())
+la3sdir = os.path.split(os.path.realpath(__file__))[0]
+
+segment_template = Template(open(la3sdir + '/per_segment.template','r').read())
 
 with open("%s/segments.json" % config.data_dir,'r') as f:
     segments = json.load(f)
@@ -11,17 +14,20 @@ with open("%s/segments.json" % config.data_dir,'r') as f:
 loci = sorted(segments.keys())
 
 for i in range(0,len(loci)-1):
-    (chrom, from_kb)    = loci[i].split(':')
-    (next_chrom, to_kb) = loci[i+1].split(':')
+    (chrom, from_bp)    = loci[i].split(':')
+    (next_chrom, to_bp) = loci[i+1].split(':')
     if chrom == next_chrom:
-        with open("%s_%s.job" % (chrom,from_kb), "w") as f:
+        with open("%s_%s.job" % (chrom,from_bp), "w") as f:
             f.write( segment_template.render( plink   = config.plink,
                                               emmax   = config.emmax,
                                               metal   = config.metal,
                                               python  = config.python,
+                                              rundir  = config.rundir,
+                                              la3sdir = la3sdir,
                                               inputs     = config.inputs,
                                               segments = "%s/segments.json" % config.data_dir,
                                               chrom      = chrom,
-                                              from_kb    = from_kb,
-                                              to_kb      = to_kb, ) )
+                                              from_bp    = from_bp,
+                                              to_bp      = to_bp, ) )
+            os.chmod(f.name, 0755)
 
