9aa5508cd4394552f8e130390d48397130241ef2
absorto <rgarcia@inmegen.gob.mx>
2015-01-20 16:35:11 -0600

    do not overlap regions

--- create_jobs.py
+++ create_jobs.py
@@ -16,6 +16,7 @@ loci = sorted(segments.keys())
 for i in range(0,len(loci)-1):
     (chrom, from_bp)    = loci[i].split(':')
     (next_chrom, to_bp) = loci[i+1].split(':')
+    to_bp = str(int(to_bp) - 1)
     if chrom == next_chrom:
         with open("%s_%s.job" % (chrom,from_bp), "w") as f:
             f.write( segment_template.render( plink   = config.plink,
