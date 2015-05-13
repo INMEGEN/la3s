f4dfc03a7b2a86cee3bffa7e406a8b3303b99514
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-05-13 12:00:05 -0500

    update a bunch of paths for different host

--- GWI/condor_make_scripts.py
+++ GWI/condor_make_scripts.py
@@ -6,24 +6,24 @@ Arguments = metaimpute.py \\
           --chr $chr \\
           --start $start \\
           --end $end \\
-          --ped /home/rgarcia/la3s/GWI/data/INDIGENAS_GWAS_519_TODASPOB.QC.UNIF.ped \\
-          --map /home/rgarcia/la3s/GWI/data/INDIGENAS_GWAS_519_TODASPOB.QC.UNIF.map \\
-          --hdlfam /home/rgarcia/la3s/GWI/data/logHDL_GWAS.fam \\
-          --ldlfam /home/rgarcia/la3s/GWI/data/logLDL_GWAS.fam \\
-          --cov /home/rgarcia/la3s/GWI/data/covariables+IMC_GWAS.txt \\
-          --combined_mask /home/rgarcia/la3s/GWI/1000GP_Phase3/genetic_map_chr%s_combined_b37.txt \\
-          --hap_mask /home/rgarcia/la3s/GWI/1000GP_Phase3/1000GP_Phase3_chr%s.hap.gz \\
-          --legend_mask /home/rgarcia/la3s/GWI/1000GP_Phase3/1000GP_Phase3_chr%s.legend.gz \\
-          --outdir /home/rgarcia/la3s/GWI/out \\
-          --log /home/rgarcia/la3s/GWI/out/impute_${chr}_${start}_${end}.log
-Error = /home/rgarcia/la3s/GWI/out/condor_${chr}_${start}_${end}.log
-Requirements = Machine == "gaia.inmegen.gob.mx"
+          --ped /state/partition1/home/rgarcia/la3s/GWI/data1/INDIGENAS_GWAS_519_TODASPOB.QC.UNIF.GWI.ped \\
+          --map /state/partition1/home/rgarcia/la3s/GWI/data1/INDIGENAS_GWAS_519_TODASPOB.QC.UNIF.GWI.map \\
+          --hdlfam /export/home/rgarcia/la3s/GWI/data/logHDL_GWAS.fam \\
+          --ldlfam /export/home/rgarcia/la3s/GWI/data/logLDL_GWAS.fam \\
+          --cov /export/home/rgarcia/la3s/GWI/data/covariables+IMC_GWAS.txt \\
+          --combined_mask /export/home/rgarcia/la3s/GWI/1000GP_Phase3/genetic_map_chr%s_combined_b37.txt \\
+          --hap_mask /export/home/rgarcia/la3s/GWI/1000GP_Phase3/1000GP_Phase3_chr%s.hap.gz \\
+          --legend_mask /export/home/rgarcia/la3s/GWI/1000GP_Phase3/1000GP_Phase3_chr%s.legend.gz \\
+          --outdir /export/home/rgarcia/la3s/GWI/out \\
+          --log /export/home/rgarcia/la3s/GWI/out/impute_${chr}_${start}_${end}.log
+Error = /export/home/rgarcia/la3s/GWI/out/condor_${chr}_${start}_${end}.log
+Requirements = Machine == "muuk.inmegen.gob.mx"
 Queue
 ''')
 
 
 
-print "executable = /usr/bin/python"
+print "executable = /share/apps/Python-2.7.3/bin/python"
 print "universe = vanilla"
 
 chr_boundaries = json.load(open('chr_boundaries.json','r'))
